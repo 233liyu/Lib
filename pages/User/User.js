64ab8f6dd9dea46fa6bba7ef84338018626cb0ac
VivianSu <sssucq@sina.com>
2017-05-16 15:39:02 +0800

    5.16

--- pages/User/User.js
+++ pages/User/User.js
@@ -1,34 +1,99 @@
+var app = getApp()
+var pp = null
+function requestUserInfo(that) {
+  // 拉起获取用户信息的请求
+  console.log(that)
+  wx.login({
+    success: function () {
+      wx.getUserInfo({
+        success: function (res) {
+          console.log('成功拉起授权')
+          app.globalData.userInfo = res.userInfo
+          that.setData({
+            userInfo: res.userInfo
+          })
+        },
+        fail: function () {
+          console.log('获取用户信息失败')
+          wx.redirectTo({
+            url: '/pages/Login/CellPhone',
+            success: function (res) {
+              // success
+            },
+            fail: function (res) {
+              // fail
+            },
+            complete: function (res) {
+              // complete
+            }
+          })
+
+        }
+      })
+    },
+    fail: function () {
+      console.log('log in failed')
+    }
+  })
+}
 Page({
   data:{
-   
+   userInfo:null,
   },
+  
   onLoad:function(options){
     // 生命周期函数--监听页面加载
-   
+    console.log("user:onload")
+    if(app.globalData.userInfo == null){
+      console.log("user:is not logged")
+      wx.navigateTo({
+        url: '/pages/Login/LoginMain',
+      })
+
+    } else {
+      console.log("user:is logged")
+      var user = app.globalData.userInfo;
+      this.setData({
+        userInfo: user,
+      })
+      console.log("user:")
+      console.log(user)
+    }
+    
   },
+  
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
     
   },
   onShow:function(){
     // 生命周期函数--监听页面显示
-    
+    if (app.globalData.userInfo == null) {
+      console.log("user:is not logged")
+      wx.navigateTo({
+        url: '/pages/Login/LoginMain',
+      })
+
+    } else {
+      console.log("user:is logged")
+      var user = app.globalData.userInfo;
+      this.setData({
+        userInfo: user,
+      })
+      console.log("user:")
+      console.log(user)
+    }
   },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-   
+  ToFailed:function(that){
+    wx.navigateTo({
+      url: '../orders/unfinishedOrder/unfinishedOrder',
+    })
+
   },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-   
-  },
-  onPullDownRefresh: function() {
-    // 页面相关事件处理函数--监听用户下拉动作
-    
-  },
-  onReachBottom: function() {
-    // 页面上拉触底事件的处理函数
-  
+  ToSuccess:function(that){
+    wx.navigateTo({
+      url: '../orders/finishedOrder/finishedOrder',
+    })
   },
   onShareAppMessage: function() {
     // 用户点击右上角分享
